bd370dec99587d5cd6e64d69009491f71389866f
Yassin raki <68440584+yaasin-raki2@users.noreply.github.com>
2020-12-12 15:57:20 +0100

    adding some Big O analytics

--- sortingAlgorithms.js
+++ sortingAlgorithms.js
@@ -2,10 +2,10 @@
 
 function bubbleSort(arr){
    var len = arr.length;
-   for (var i = len-1; i>=0; i--){
-     for(var j = 1; j<=i; j++){
+   for (let i = len-1; i>=0; i--){
+     for(let j = 1; j<=i; j++){
        if(arr[j-1]>arr[j]){
-           var temp = arr[j-1];
+           let temp = arr[j-1];
            arr[j-1] = arr[j];
            arr[j] = temp;
         }
@@ -13,6 +13,4 @@ function bubbleSort(arr){
    }
    return arr;
 }
-bubbleSort([7,5,2,4,3,9]); //[2, 3, 4, 5, 7, 9]
-bubbleSort([9,7,5,4,3,1]); //[1, 3, 4, 5, 7, 9]
-bubbleSort([1,2,3,4,5,6]); //[1, 2, 3, 4, 5, 6]
+bubbleSort([7,5,2,4,3,9]); // Big(O): -Time Complexity O(n^2) / -Space Complexity O(1) | (So freaking expensive) => {I hate bubble sort -_-}
