e3eb68f7d3e9de445952d1a47c920433945cf7d9
Yassin raki <68440584+yaasin-raki2@users.noreply.github.com>
2020-12-13 22:14:39 +0100

    ES6*

--- sortingAlgorithms.js
+++ sortingAlgorithms.js
@@ -1,6 +1,6 @@
 // bubbleSort methode
 const bubbleSort = arr => {
-   var len = arr.length;
+   const len = arr.length;
    for (let i = len-1; i>=0; i--){
      for(let j = 1; j<=i; j++){
        if(arr[j-1]>arr[j]){
