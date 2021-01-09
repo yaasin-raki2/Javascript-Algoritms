1811d559ae0c77b0f07204db40a9ab10c5e4a405
yaasin-raki2 <yassin23raki@gmail.com>
2021-01-10 00:52:00 +0100

    added another implementation of the brute force solution of the TwoSum problem

--- TwoSum.js
+++ TwoSum.js
@@ -1,5 +1,5 @@
 // Brute fore Nested Loop
-const indecies = (target, numbers) => {
+const twoSum = (target, numbers) => {
   for (let i = 0; i < numbers.length; i++) {
     for (let j = i + 1; j < numbers.length; j++) {
       if (numbers[i] + numbers[j] === target) {
@@ -10,10 +10,25 @@ const indecies = (target, numbers) => {
   return null;
 };
 
-console.log(indecies(7, [1, 2, 5])); // Big(O): - Time Complexity: O(n^2) / - Space Complexity: O(1)
+console.log(twoSum(7, [1, 2, 5])); // Big(O): - Time Complexity: O(n^2) / - Space Complexity: O(1)
+
+// Another  implementationn of the Brute force solution
+const twoSum2 = (target, numbers) => {
+  for (let p1 = 0; p1 < numbers.length; p1++) {
+    const numberToFind = target - numbers[p1];
+    for (let p2 = p1 + 1; p2 < numbers.length; p2++) {
+      if (numbers[p2] === numberToFind) {
+        return [p1, p2];
+      }
+    }
+  }
+  return null;
+};
+
+console.log(twoSum2(7, [1, 2, 5])); // Big(O): - Time Complexity: O(n^2) / - Space Complexity: O(1)
 
 // Using Hash Tabels to reduce the Time Complexity
-const sum = (target, numbers) => {
+const twoSum3 = (target, numbers) => {
   let obj = {};
   for (let i = 0; i < numbers.length; i++) {
     obj[target - numbers[i]] = i;
@@ -24,4 +39,4 @@ const sum = (target, numbers) => {
   return null;
 };
 
-console.log(sum(7, [1, 2, 4, 3, 5])); // Big(O): - Time Complexity: O(n) / - Space Complexity: O(n)
+console.log(twoSum3(7, [1, 2, 4, 3, 5])); // Big(O): - Time Complexity: O(n) / - Space Complexity: O(n)
