98170127472e13d77aa59ddb9adb976084ef6182
yaasin-raki2 <yassin23raki@gmail.com>
2021-04-11 13:08:44 +0000

    Added an Optimal solution O(a+b)/O(1) for the Hashtags problem

--- Hashtags.js
+++ Hashtags.js
@@ -23,4 +23,49 @@ const Hashtags = (S, T) => {
   }
 };
 
-console.log(Hashtags("asi##d", "af#d"));
+console.log(Hashtags("asi##d", "af#d")); // Big(O): - Time Complexity: O(a+b) / - Space Complexity: O(a+b)
+
+const Hashtags2 = (S, T) => {
+  let p1 = S.length - 1;
+  let p2 = T.length - 1;
+
+  let result = false;
+
+  while (p1 >= 0 || p2 >= 0) {
+    if (S[p1] === "#" || T[p2] === "#") {
+      if (S[p1] === "#") {
+        let backCount = 2;
+        while (backCount > 0) {
+          p1--;
+          backCount--;
+          if (S[p1] === "#") {
+            backCount += 2;
+          }
+        }
+      }
+      if (T[p2] === "#") {
+        let backCount = 2;
+        while (backCount > 0) {
+          p2--;
+          backCount--;
+          if (T[p2] === "#") {
+            backCount += 2;
+          }
+        }
+      }
+    }
+
+    if (S[p1] !== T[p2]) {
+      result = false;
+    } else if (S[p1] === T[p2]) {
+      result = true;
+    }
+
+    p1--;
+    p2--;
+  }
+
+  return result;
+};
+
+console.log(Hashtags2("ask##d", "af#d")); // Big(O): - Time Complexity: O(a+B) / - Space Complexity: O(1)
