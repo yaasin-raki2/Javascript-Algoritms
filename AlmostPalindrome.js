6d5634504be7a459a94575fd147cdd155aec4f99
yaasin-raki2 <yassin23raki@gmail.com>
2021-04-12 15:04:54 +0000

    Adding another Optimal Solution for the AlmostPalindrome Problem

--- AlmostPalindrome.js
+++ AlmostPalindrome.js
@@ -36,3 +36,33 @@ const AlmostPalindrome = (S) => {
 };
 
 console.log(AlmostPalindrome("abccdba")); // Big(O): - Time Complexity: O(n) / - Space Complexity: O(1)
+
+const AlmostPalindrome2 = (S) => {
+  S = S.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+
+  let p1 = 0;
+  let p2 = S.length - 1;
+
+  const isValisSubPalindrome = (S, p1, p2) => {
+    while (p1 < p2) {
+      if (S[p1] !== S[p2]) {
+        return false;
+      }
+      p1++;
+      p2--;
+    }
+    return true;
+  };
+
+  while (p1 < p2) {
+    if (S[p1] !== S[p2]) {
+      return isValisSubPalindrome(S, p1 + 1, p2) || isValisSubPalindrome(S, p1, p2 - 1);
+    }
+    p1++;
+    p2--;
+  }
+
+  return true;
+};
+
+console.log(AlmostPalindrome2("abccdba")); // Big(O): - Time Complexity: O(n) / - Space Complexity: O(1)
