de09a927465f9adc195158b17c5041f84dbcc85f
yaasin-raki2 <yassin23raki@gmail.com>
2021-04-15 12:09:27 +0000

    Added a basic implementation of a Doubly Linked List

---
+++ DoublyLinkedList.js
@@ -0,0 +1,29 @@
+export class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+export class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+
+    return this;
+  }
+}
