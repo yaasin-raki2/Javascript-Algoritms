35da0d94f51b0165d279b3ade76ba393a26c077f
yaasin-raki2 <yassin23raki@gmail.com>
2021-04-16 14:42:43 +0000

    Added two more solution of the LinkedListCycle Detection problem using the Floyd's Tortoise and hare algorithm

--- LinkedListCycleDetection.js
+++ LinkedListCycleDetection.js
@@ -16,3 +16,58 @@ const LinkedListCycleDetection = (head) => {
 };
 
 LinkedListCycleDetection(); // Big(O): - Time Complexity: O(n) / - Space Complexity: O(n)
+
+// Using the Floyd's Tortoise and Hare Algorithm
+const LinkedListCycleDetection2 = (head) => {
+  let tortoise = head.next;
+  let hare = head.next.next;
+
+  while (tortoise !== hare) {
+    if (!hare.next || !hare) return false;
+
+    tortoise = tortoise.next;
+    hare = hare.next.next;
+  }
+
+  tortoise = head;
+
+  while (tortoise !== hare) {
+    tortoise = tortoise.next;
+    hare = hare.next;
+  }
+
+  return tortoise;
+};
+
+LinkedListCycleDetection(); // Big(O): - Time Complexity: O(n) / - Space Complexity: O(1)
+
+// Another implementation
+const LinkedListCycleDetection2 = (head) => {
+  let hare = head;
+  let tortoise = head;
+
+  while (true) {
+    hare = hare.next;
+    tortoise = tortoise.next;
+
+    if (!hare || !hare.next) {
+      return false;
+    } else {
+      hare = hare.next;
+    }
+
+    if (tortoise === hare) break;
+  }
+
+  let p1 = head;
+  let p2 = tortoise;
+
+  while (p1 !== p2) {
+    p1 = p1.next;
+    p2 = p2.next;
+  }
+
+  return p1;
+};
+
+LinkedListCycleDetection3(); // Big(O): - Time Complexity: O(n) / - Space Complexity: O(1)
