8783dcde3059889a146d7a9bedd68f363c6ccc97
yaasin-raki2 <yassin23raki@gmail.com>
2021-04-18 10:41:28 +0000

    Added a new sorting algorithm: Merge Sort

---
+++ MergeSort.js
@@ -0,0 +1,31 @@
+const MergeSort = (arr) => {
+  if (arr.length === 1) {
+    return arr;
+  }
+
+  const middle = Math.floor(arr.length / 2);
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle);
+
+  const Merge = (left, right) => {
+    const result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+        result.push(left[leftIndex]);
+        leftIndex++;
+      } else {
+        result.push(right[rightIndex]);
+        rightIndex++;
+      }
+    }
+
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+  };
+
+  return Merge(MergeSort(left), MergeSort(right));
+};
+
+console.log(MergeSort([1, 3, 2, 6, 4, 5]));
