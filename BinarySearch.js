fe1f3e3ce85a752668701869b9538fe4de4e0abc
yaasin-raki2 <yassin23raki@gmail.com>
2021-04-18 16:16:55 +0000

    Added another Solution for Binary Search

--- BinarySearch.js
+++ BinarySearch.js
@@ -22,3 +22,28 @@ const BinarySearch = (arr, target, left = 0, right = arr.length - 1) => {
 // Big(O): - Time Complexity: O(log(n)) / - Space Complexity: O(1) When Tail Recursion is available
 
 console.log(BinarySearch([1, 2, 3, 4, 5, 6, 7, 8], 3));
+
+// With Loops
+const BinarySearch2 = (arr, target) => {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const foundValue = arr[mid];
+
+    if (target === foundValue) {
+      return mid;
+    } else if (target > foundValue) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+};
+
+// Big(O): - Time Complexity: O(log(n)) / - Space Complexity: O(1)
+
+console.log(BinarySearch2([1, 2, 3, 4, 5, 6, 7, 8], 3));
