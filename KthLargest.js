604102b32212332d19fd8725ccb5c97891739cab
yaasin-raki2 <yassin23raki@gmail.com>
2021-04-18 15:21:58 +0000

    Added another Optimal Solution for the Kth largest problem using Quick Selection

--- KthLargest.js
+++ KthLargest.js
@@ -21,7 +21,6 @@ const KthLargest = (arr, k) => {
 console.log(KthLargest([1, 3, 2, 7, 9, 5], 2)); // Big(O): - Time Complexity: O(n^2) / - Space Complexity: O(1)
 
 // Oprimal Solution using Quick Sort
-
 const KthLargest2 = (arr, k) => {
   const QuickSort = (arr, left = 0, right = arr.length - 1) => {
     const swap = (arr, i, j) => {
@@ -59,3 +58,44 @@ const KthLargest2 = (arr, k) => {
 };
 
 console.log(KthLargest2([1, 3, 2, 7, 9, 5], 2)); // Big(O): - Time Complexity: O(n log(n)) / - Space Complexity: O(log(n))
+
+// More Freacking Optimal Solution using Quick Selection '-'
+const KthLargest3 = (arr, k, left = 0, right = arr.length - 1) => {
+  const swap = (arr, i, j) => {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  };
+
+  const partition = (arr, left, right) => {
+    const pivotElement = arr[right];
+    let partitionIdx = left;
+
+    for (let j = partitionIdx; j < right; j++) {
+      if (arr[j] < pivotElement) {
+        swap(arr, partitionIdx, j);
+        partitionIdx++;
+      }
+    }
+
+    swap(arr, partitionIdx, right);
+
+    return partitionIdx;
+  };
+
+  if (left < right) {
+    const partitionIdx = partition(arr, left, right);
+
+    if (partitionIdx === arr.length - k) {
+      return arr[arr.length - k];
+    } else if (arr.length - k > partitionIdx) {
+      return KthLargest3(arr, k, partitionIdx + 1, right);
+    } else {
+      return KthLargest(arr, k, left, partitionIdx - 1);
+    }
+  }
+
+  return arr[arr.length - k];
+};
+
+console.log(KthLargest3([1, 3, 2, 7, 9, 5], 2)); // Big(O): - Time Complexity: BestCase O(n) | WorstCase O(n^2) / - Space Complexity: O(1) With Tail Reacursion Enabled
